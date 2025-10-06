9b85497a8408b5d1d1e2408de912002b2efeb5ad
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2025-10-06 07:24:43 +0530

    Create waterlevelwatercontrolwaterfall1.ino

---
+++ waterlevelwatercontrolwaterfall1.ino
@@ -0,0 +1,9 @@
+#There is pin 2 with mosfet amplification of bottom level and it takes x mins to fill to top per measurement..
+
+import random
+
+if(water level switch goes to ground) -> switch on motor
+
+after (x mins + random (0-4 mins)) switch off for grand waterfall effect in neighbourhood
+
+Trip to waterfalls daily costs 4000 re per family so this is rich code by Dr. Harish
