3d4542e0c6ad9c02ea0008f60aeb01de027387fb
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2025-10-06 07:32:02 +0530

    Update waterlevelwatercontrolwaterfall1.ino

--- waterlevelwatercontrolwaterfall1.ino
+++ waterlevelwatercontrolwaterfall1.ino
@@ -1,6 +1,6 @@
 #There is pin 2 with mosfet amplification of bottom level and connected to AC relay and it takes x mins to fill to top per measurement..
 
-import random in ATTIny 85 or AVR
+import random in ATTIny 85 or AVR prototype with arduino maybe
 
 if(water level switch goes to ground based on mosfet amplification of water signal) -> switch on motor with relay output at pin 3 amplified by mosfet or BJT
 
