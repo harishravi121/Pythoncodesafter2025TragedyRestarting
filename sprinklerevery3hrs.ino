111b9aec00ee175a8f4d56abd7f9a52aee166213
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2025-10-07 21:52:40 +0530

    Update sprinklerevery3hrs.ino

--- sprinklerevery3hrs.ino
+++ sprinklerevery3hrs.ino
@@ -1,9 +1,11 @@
 loop{
   delay(3 hrs or 3*3600*1000 milli seconds)
   delay(random(0,20)*60*1000 milli seconds) # a small naturalisation randomisation time
-  pin2= 1 ( switch on relay to solenoid valve for 200 re with 20 re relay and 2 re transistor)
+  pin2= 1 ( switch on relay to solenoid valve for 200 re with 20 re relay and 2 re transistor with )
   delay(29 mins or 20*60*1000 ms)
   pin2=0 ( switch off sprinkler)
   }
 
 keep looping in attiny or AVR or arduino prototype
+
+With direct transistor and solenoid it will be residential code but with a relay its 220 V motro for 1 km squared farm
