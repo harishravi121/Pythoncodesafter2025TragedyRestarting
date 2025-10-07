0e6478cd5e6b10194b5f304a0ffbace4ef9614fa
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2025-10-07 21:15:12 +0530

    Update sprinklerevery3hrs.ino

--- sprinklerevery3hrs.ino
+++ sprinklerevery3hrs.ino
@@ -1,5 +1,6 @@
 loop{
-  delay(4 hrs or 4*3600*1000 milli seconds)
+  delay(3 hrs or 3*3600*1000 milli seconds)
+  delay(random(0,20)*60*1000 milli seconds) # a small naturalisation randomisation time
   pin2= 1 ( switch on relay to solenoid valve for 200 re with 20 re relay and 2 re transistor)
   delay(29 mins or 20*60*1000 ms)
   pin2=0 ( switch off sprinkler)
