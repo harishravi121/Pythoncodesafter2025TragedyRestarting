e7d99eb95b23fe473d7599d2e128d20329429b48
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2025-10-07 21:12:39 +0530

    Create sprinklerevery3hrs.ino

---
+++ sprinklerevery3hrs.ino
@@ -0,0 +1,8 @@
+loop{
+  delay(4 hrs or 4*3600*1000 milli seconds)
+  pin2= 1 ( switch on relay to solenoid valve for 200 re with 20 re relay and 2 re transistor)
+  delay(29 mins or 20*60*1000 ms)
+  pin2=0 ( switch off sprinkler)
+  }
+
+keep looping
