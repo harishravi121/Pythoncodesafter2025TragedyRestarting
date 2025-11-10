7cc360a57bbafc8862250aa745209d6f367c6765
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2025-11-10 13:48:48 +0530

    Create beautifulwater.ino for automated watering
    Initial implementation of a watering system using LED as an indicator.

---
+++ beautifulwater.ino
@@ -0,0 +1,37 @@
+something0=2 mins
+//watering plants and filling tanks cheap
+somethig=5 mins
+LED BUILTIN means wire connected to motor also..
+void setup() {
+  
+  //LED should be connected to transistor and relay for pump and a for loop for 1000 should be written around delay as its in milliseconds
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop routine runs over and over again forever: this delay is in ms or seconds it doesn't matter much a random prefactor can be added too for beauty
+void loop() {
+  long delayoff=3*60*3600+random(something0);
+  long delayon=20+random(something);
+
+  delay(delayoff);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayon);
+  digitalWrite(LED_BUILTIN, LOW);
+  }
+
+//can add some random delays as cushions in between appropriately some waterfall effect is also good
+//one can make a verilog code to directly make it at factory with ai and as small as possible 
+
+//Trip to waterfalls daily costs 4000 re per family so this is rich code by Dr. Harish
+
+
+//  THe water must fall to a slanting garden or trees or its okay with rainwater harvesting but it must be audible..
+
+ // If you add a plastic ball float with timer plus random you can avoid waterfall losses too..
+
+This needs no wiring to top floor  and climbing etc..
+
+
+
+
+
